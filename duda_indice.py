44743c9499570fd2836d7a1c47fc35888829c17a
HighLevelofCode <66216113+HighLevelofCode@users.noreply.github.com>
2020-05-31 14:14:25 -0500

    Add files via upload

---
+++ duda_indice.py
@@ -0,0 +1,11 @@
+l = ['Larry','Juan','Miguel','Rosa','Maria']
+
+#DUDA: David Montero
+# cuando adquiero el índice de un elemento no existente  
+# en una colección ¿que debería pasar y por qué? Gracias
+
+print(l[6])
+
+# Cuando colocamos un indice que sobrepasa el de los valores establecidos
+# obtenemos un error de tipo INDEX el cual nos avisa que
+# el indice que hemos colocado se encuentra fuera del rango permitido
